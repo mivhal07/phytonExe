fa4000227ccd79973d990988f54875ea76db7c51
mivhal07 <michaldob07@gmail.com>
2022-02-27 16:48:27 +0100

    ćwiczenia 27.02.2022

---
+++ pythonProject/cwiczenia27_02_2022/Main03.py
@@ -0,0 +1,8 @@
+for a in range(0, 7, 1):
+    print(a)
+
+lista = ['a', 3, 4, 5.6]
+for element in lista:
+    print(element)
+else:
+    print('już wszystko')
---
+++ pythonProject/cwiczenia27_02_2022/Main04.py
@@ -0,0 +1,6 @@
+licznik = 0
+while licznik != 11:
+    print(licznik)
+    licznik += 1
+else:
+    print('zostało wyświetlonych ' + str(licznik) + ' liczb')
